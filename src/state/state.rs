b897133590fcedc63686cdd5c939f449c89f6d30
Augusto Teixeira <augusto@impa.br>
2023-06-26 08:15:53 +0100

    Start ownership to state

--- src/state/state.rs
+++ src/state/state.rs
@@ -32,38 +32,64 @@ pub enum Team {
     Red,
 }
 
-pub type EntityArray = Vec<FullEntity>;
-
-pub struct Tile {
+pub struct SerialTile {
     pub materials: Materials,
     pub entity_index: Option<usize>,
 }
 
-pub struct State {
+pub struct Tile {
+    pub materials: Materials,
+    pub entity: Option<Box<FullEntity>>,
+}
+
+pub struct SerialState {
     pub codes: [Code; NUM_CODES],
-    pub entities: EntityArray,
+    pub entities: Vec<FullEntity>,
     pub blue_templates: [FullEntity; NUM_SUB_ENTITIES],
     pub red_templates: [FullEntity; NUM_SUB_ENTITIES],
-    pub tiles: [Tile; WIDTH * HEIGHT],
+    pub tiles: [SerialTile; WIDTH * HEIGHT],
 }
 
 pub struct State {
     pub codes: [Code; NUM_CODES],
-    pub entities: EntityArray,
     pub blue_templates: [FullEntity; NUM_SUB_ENTITIES],
     pub red_templates: [FullEntity; NUM_SUB_ENTITIES],
     pub tiles: [Tile; WIDTH * HEIGHT],
 }
-
-// SEND TO ANOTHER FILE
 
 #[derive(Debug, Snafu)]
 pub enum StateError {
-    #[snafu(display("No entity in tile {:?}", pos))]
-    NoEntityInTile { pos: Pos },
+    #[snafu(display("Cannot move entity from empty tile {:?}", pos))]
+    MoveFromEmptyTile { pos: Pos },
+    #[snafu(display("Cannot move entity to occupied tile {:?}", pos))]
+    MoveToOccupiedTile { pos: Pos },
     #[snafu(display("Entity in tile {:?}", pos))]
     EntityInTile { pos: Pos },
 }
+
+impl State {
+    pub fn has_entity(&self, pos: Pos) -> bool {
+        self.tiles[pos.to_index()].entity.is_some()
+    }
+    pub fn move_entity(
+        &mut self,
+        from: Pos,
+        to: Pos,
+    ) -> Result<(), StateError> {
+        if !self.has_entity(from) {
+            return Err(StateError::MoveFromEmptyTile { pos: from });
+        }
+        if self.has_entity(to) {
+            return Err(StateError::MoveToOccupiedTile { pos: to });
+        }
+        let entity = self.tiles.get_mut(from.to_index()).unwrap().entity.take();
+        self.tiles[from.to_index()].entity = None;
+        self.tiles[to.to_index()].entity = entity;
+        Ok(())
+    }
+}
+
+// SEND TO ANOTHER FILE
 
 #[derive(Debug)]
 pub enum Event {
@@ -91,8 +117,12 @@ pub struct Construct {
 
 #[derive(Debug, Snafu)]
 pub enum UpdateError {
-    #[snafu(display("Moving entity from {from:?} to {to:?}: {message}"))]
-    EntityMove { from: Pos, to: Pos, message: String },
+    #[snafu(display("Moving entity from {from:?} to {to:?}"))]
+    EntityMove {
+        source: StateError,
+        from: Pos,
+        to: Pos,
+    },
     #[snafu(display("Error moving materials {from:?} to {to:?}: {message}"))]
     MaterialMove { from: Pos, to: Pos, message: String },
 }
@@ -100,85 +130,72 @@ pub enum UpdateError {
 pub fn update(state: &mut State, event: Event) -> Result<(), UpdateError> {
     match event {
         Event::EntityMove(from, to) => {
-            let entity: usize = state.tiles[from.to_index()]
-                .entity_index
-                .ok_or(UpdateError::EntityMove {
-                    from,
-                    to,
-                    message: "No entity in origin".to_string(),
-                })?;
-            if let None = state.tiles[to.to_index()].entity_index {
-                return Err(UpdateError::EntityMove {
-                    from,
-                    to,
-                    message: "Moving to occupied tile".to_string(),
-                });
-            }
-            state.tiles[to.to_index()].entity_index = Some(entity);
-            state.tiles[from.to_index()].entity_index = None;
+            state
+                .move_entity(from, to)
+                .context(EntityMoveSnafu { from, to })?;
         }
-        Event::AssetsEntityToFloor(payload, from, to) => {
-            let entity: usize = state.tiles[from.to_index()]
-                .entity_index
-                .ok_or(UpdateError::EntityMove {
-                    from,
-                    to,
-                    message: "No entity in origin".to_string(),
-                })?;
-            let assets_on_entity = state.entities[entity].materials.clone();
-            ensure!(
-                assets_on_entity.ge(&payload),
-                MaterialMoveSnafu {
-                    from: from,
-                    to: to,
-                    message: format!(
-                        "Entity has {assets_on_entity:?}, not ({payload:?})"
-                    )
-                }
-            );
-            state.entities[entity].materials -= payload.clone();
-            state.tiles[to.to_index()].materials += payload;
-        }
-        Event::AssetsFloorToEntity(payload, from, to) => {
-            let assets_on_floor =
-                state.tiles[from.to_index()].materials.clone();
-            let entity: usize = state.tiles[to.to_index()].entity_index.ok_or(
-                UpdateError::MaterialMove {
-                    from,
-                    to,
-                    message: "No entity in destination".to_string(),
-                },
-            )?;
-            ensure!(
-                assets_on_floor.ge(&payload),
-                MaterialMoveSnafu {
-                    from: from,
-                    to: to,
-                    message: format!("Not enough material ({payload:?}) in {assets_on_floor:?}")
-                }
-            );
-            ensure!(
-                payload.volume() + state.entities[entity].materials.volume()
-                    <= state.entities[entity].inventory_size,
-                MaterialMoveSnafu {
-                    from: from,
-                    to: to,
-                    message: format!("Not enough capacity for {payload:?}")
-                }
-            );
-            state.entities[entity].materials += payload.clone();
-            state.tiles[to.to_index()].materials -= payload;
-        }
-        Event::Shoot(a) => {
-            if let Some(e) = state.tiles[a.destination.to_index()].entity_index
-            {
-                if state.entities[e].max_hp <= a.damage {
-                    //state.entities[e]
-                }
-            }
-        }
-        Event::Drill(_a) => {}
-        Event::Construct(_c) => {}
+        //         Event::AssetsEntityToFloor(payload, from, to) => {
+        //             let entity = state.tiles[from.to_index()].entity.ok_or(
+        //                 UpdateError::EntityMove {
+        //                     from,
+        //                     to,
+        //                     message: "No entity in origin".to_string(),
+        //                 },
+        //             )?;
+        //             let assets_on_entity = entity.materials.clone();
+        //             ensure!(
+        //                 assets_on_entity.ge(&payload),
+        //                 MaterialMoveSnafu {
+        //                     from: from,
+        //                     to: to,
+        //                     message: format!(
+        //                         "Entity has {assets_on_entity:?}, not ({payload:?})"
+        //                     )
+        //                 }
+        //             );
+        //             entity.materials -= payload.clone();
+        //             state.tiles[to.to_index()].materials += payload;
+        //         }
+        //         Event::AssetsFloorToEntity(payload, from, to) => {
+        //             let assets_on_floor =
+        //                 state.tiles[from.to_index()].materials.clone();
+        //             let entity = state.tiles[to.to_index()].entity.ok_or(
+        //                 UpdateError::MaterialMove {
+        //                     from,
+        //                     to,
+        //                     message: "No entity in destination".to_string(),
+        //                 },
+        //             )?;
+        //             ensure!(
+        //                 assets_on_floor.ge(&payload),
+        //                 MaterialMoveSnafu {
+        //                     from: from,
+        //                     to: to,
+        //                     message: format!("Not enough material ({payload:?}) in {assets_on_floor:?}")
+        //                 }
+        //             );
+        //             ensure!(
+        //                 payload.volume() + entity.materials.volume()
+        //                     <= entity.inventory_size,
+        //                 MaterialMoveSnafu {
+        //                     from: from,
+        //                     to: to,
+        //                     message: format!("Not enough capacity for {payload:?}")
+        //                 }
+        //             );
+        //             entity.materials += payload.clone();
+        //             state.tiles[to.to_index()].materials -= payload;
+        //         }
+        //         Event::Shoot(a) => {
+        //             if let Some(entity) = state.tiles[a.destination.to_index()].entity {
+        //                 if entity.max_hp <= a.damage {
+        //                     //state.entities[e]
+        //                 }
+        //             }
+        //         }
+        //         Event::Drill(_a) => {}
+        //         Event::Construct(_c) => {}
+        _ => {}
     }
     Ok(())
 }
